ec6afc060cb68198264e2d8068c1dd269a952a71
Ubuntu <ubuntu@ip-172-31-43-161.ec2.internal>
2020-02-27 01:04:51 +0000

    Completed lab5

--- app/views.py
+++ app/views.py
@@ -31,38 +31,54 @@ def about():
 
 @app.route("/login", methods=["GET", "POST"])
 def login():
+    if current_user.is_authenticated:
+        return redirect(url_for('secure_page'))
     form = LoginForm()
     if request.method == "POST" and form.validate_on_submit():
         # change this to actually validate the entire form submission
         # and not just one field
         # Get the username and password values from the form.
-        username = form.username.data
-        password = form.password.data
-        # using your model, query database for a user based on the username
-        # and password submitted. Remember you need to compare the password hash.
-        # You will need to import the appropriate function to do so.
-        # Then store the result of that query to a `user` variable so it can be
-        # passed to the login_user() method below.
-        user = UserProfile.query.filter_by(username=username, password=password).first()
-        # get user id, load into session
-        if user is not None and check_password_hash(user.password, password):
-            login_user(user)
+        if form.username.data:
+            username = form.username.data
+            password = form.password.data
+            # using your model, query database for a user based on the username
+            # and password submitted. Remember you need to compare the password hash.
+            # You will need to import the appropriate function to do so.
+            # Then store the result of that query to a `user` variable so it can be
+            # passed to the login_user() method below.
+            user = UserProfile.query.filter_by(username=username).first()
+            # get user id, load into session
+            if user is not None and check_password_hash(user.password, password):
+                login_user(user)
                 
-            flash('Logged in sucessfully!', 'success')
-
-            # remember to flash a message to the user
-            return redirect(url_for("secure_page"))  # they should be redirected to a secure-page route instead
-        else:
-            flash('Username or Password is incorrect.', 'danger')
+                flash('You have been logged in sucessfully!', 'success')
+                # remember to flash a message to the user
+                return redirect(url_for('secure_page'))  # they should be redirected to a secure-page route instead
+            else:
+                flash('The Username or Password is incorrect. Please try again', 'danger')
     return render_template("login.html", form=form)
 
 
+@app.route('/secure-page')
+@login_required
+def secure_page():
+    """Render a secure page on our website that only logged in users can access."""
+    return render_template('secure_page.html')
+    
+@app.route('/logout')
+@login_required
+def logout():
+    """ Render home page after user has logged out """
+    logout_user()
+    flash('You have been successfully logged out.', 'success')
+    return redirect(url_for('home'))
+    
 # user_loader callback. This callback is used to reload the user object from
 # the user ID stored in the session
 @login_manager.user_loader
 def load_user(id):
     return UserProfile.query.get(int(id))
-
+    
 ###
 # The functions below should be applicable to all Flask apps.
 ###
